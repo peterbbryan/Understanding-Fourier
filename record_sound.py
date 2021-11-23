7fa9d0e59128deff2e197739a48b324fe9acd745
Peter Bryan <peterbbryan@gmail.com>
2021-11-23 00:19:35 -0500

    style: reformatting to use settings file

--- record_sound.py
+++ record_sound.py
@@ -5,6 +5,7 @@ Basic realtime fft plotting.
 import matplotlib.pyplot as plt
 import numpy as np
 import pyaudio
+from dynaconf import settings
 from scipy.ndimage import gaussian_filter1d
 
 plt.style.use("seaborn")  # switch to seaborn style
@@ -12,28 +13,42 @@ plt.style.use("seaborn")  # switch to seaborn style
 CHUNK = 1000
 _RUNNING_MAX = 1e6
 _RUNNING_MAX_VOLUME = 35000
-_FORMAT = pyaudio.paInt16
-_CHANNELS = 1
-_RATE = 10000
 
 
-def _configure_mic() -> pyaudio.Stream:
+def _configure_mic(
+    pyaudio_format: int = settings.FORMAT,
+    n_channels: int = settings.N_CHANNELS,
+    rate: int = settings.RATE,
+) -> pyaudio.Stream:
     """
+
+    Args:
+        format:
+        n_channels:
+        rate:
     """
 
-    mic = pyaudio.PyAudio()
-
-    return mic.open(
-        format=_FORMAT,
-        channels=_CHANNELS,
-        rate=_RATE,
+    return pyaudio.PyAudio().open(
+        format=pyaudio_format,
+        channels=n_channels,
+        rate=rate,
         input=True,
         output=True,
         frames_per_buffer=CHUNK,
     )
 
 
+def _fourier_transform(data):
+    """
+    """
+
+    return gaussian_filter1d(np.abs(np.fft.fft(np.frombuffer(data, np.int16))), 5)
+
+
 def _get_chunk_generator():
+    """
+    """
+
     ...
 
 
@@ -42,21 +57,30 @@ def _plot_temporal_domain(ax: plt.Axes, temporal_data: np.ndarray):
 
     Args:
         ax:
-        data:
+        temporal_data:
     """
 
     ax.plot(temporal_data)
     ax.axhline(y=0.0, color="gray", linestyle="-")
-    ax.set_xlim(0, CHUNK)
+    ax.set_xlim(0, len(temporal_data))
     ax.set_ylim(-_RUNNING_MAX_VOLUME, _RUNNING_MAX_VOLUME)
     ax.set_yticks([])
     ax.set_xticks([])
     ax.set_xlabel("")
 
 
-def _plot_frequency_domain(ax: plt.Axes, frequency_data: np.ndarray):
+def _plot_frequency_domain(
+    ax: plt.Axes, frequency_data: np.ndarray,
+):
     """
+
+    Args:
+        ax:
+        frequency_data:
     """
+
+    max_x = np.argmax(frequency_data)
+    max_y = np.max(frequency_data)
 
     ax.scatter(max_x * 10, max_y, color="red", marker="x")
     ax.text(
@@ -76,26 +100,28 @@ def _plot_frequency_domain(ax: plt.Axes, frequency_data: np.ndarray):
     ax.set_ylabel("Log-scaled magnitude")
 
 
-def _fourier_transform():
+def plot_temporal_data_and_fft():
     """
+    Args:
+        stream:
     """
 
-    ...
+    stream: pyaudio.Stream = _configure_mic()
+
+    while True:
+
+        data = stream.read(CHUNK, exception_on_overflow=False)
+        data = np.frombuffer(data, np.int16)
+        fft_data = _fourier_transform(data)
+
+        plt.clf()
+        ax1 = plt.subplot(2, 1, 1)
+        _plot_temporal_domain(ax1, data)
+        ax2 = plt.subplot(2, 1, 2)
+        _plot_frequency_domain(ax2, fft_data)
+        plt.pause(0.0000001)
 
 
-stream: pyaudio.Stream = _configure_mic()
+if __name__ == "__main__":
 
-while True:
-
-    data = stream.read(CHUNK, exception_on_overflow=False)
-    data = np.frombuffer(data, np.int16)
-    fft_data = gaussian_filter1d(np.abs(np.fft.fft(np.frombuffer(data, np.int16))), 5)
-
-    max_x = np.argmax(fft_data)
-    max_y = np.max(fft_data)
-    plt.clf()
-    ax1 = plt.subplot(2, 1, 1)
-    _plot_temporal_domain(ax1, data)
-    ax2 = plt.subplot(2, 1, 2)
-    _plot_frequency_domain(ax2, fft_data)
-    plt.pause(0.0000001)
+    plot_temporal_data_and_fft()
---
+++ settings.py
@@ -0,0 +1,6 @@
+import pyaudio
+
+FORMAT: int = pyaudio.paInt16
+N_CHANNELS: int = 1
+RATE: int = 10000
+TEMPORAL_VLIM: float = 1e6
